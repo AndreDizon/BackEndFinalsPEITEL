4f5dafc36c549824a2a861650a84ec4b472109e4
AndreDizon <cardizon.student@ua.edu.ph>
2025-12-11 14:01:48 +0800

    Fix Render deployment configuration

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l9%j*9zz^x(sn_gtb7lo5&_noe43gv7-wcgneakupl4&7)1b#^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'backendfinalspeitel.onrender.com',
